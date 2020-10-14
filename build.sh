4a42b23a317856362d5c53390797a2051b24ef01
Huang Yunkun <htynkn@gmail.com>
2020-10-14 13:14:10 +0800

    add keys

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ TARGET_FOLDER=$2;
 echo "sdkURL: ${SDKURL} targetFolder:${TARGET_FOLDER}";
 echo "building ipk ..."
 
-docker build --build-arg sdkURL=${SDKURL} --build-arg targetFolder=${TARGET_FOLDER} . -t htynkn/openwrt-switch-lan-play-${TARGET_FOLDER}:latest
+docker build -v $(pwd):/keys --build-arg sdkURL=${SDKURL} --build-arg targetFolder=${TARGET_FOLDER} . -t htynkn/openwrt-switch-lan-play-${TARGET_FOLDER}:latest
 rm -f target.zip
 docker run -v $PWD:/opt/mount --rm --entrypoint cp htynkn/openwrt-switch-lan-play-${TARGET_FOLDER}:latest /sdk/openwrt-sdk/target.zip /opt/mount/target.zip
